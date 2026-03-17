c70fc000c9afca9d3d2c39434b0ae0c6a2ade0f0
Praveen Mujawdiya <praveen.mujawdiya@gmail.com>
2026-03-17 14:38:42 +0530

    add BootstrapJS

--- assets/meili.js
+++ assets/meili.js
@@ -1,6 +1,9 @@
 import './stimulus_bootstrap.js';
 console.log('meili.js loads stimulus bootstrap and algolia css');
-import '@tabler/core';
+import * as bootstrap from 'bootstrap';
+//import '@tabler/core';
 import '@tabler/core/dist/css/tabler.min.css';
 import 'instantsearch.css/themes/algolia.min.css';
 import '@tacman1123/twig-browser';
+
+window.bootstrap = bootstrap;
