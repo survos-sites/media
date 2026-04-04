6b648bcedbf86ad18c9c73a170849b429550a83c
Tac Tacelosky <tacman@gmail.com>
2026-04-04 06:50:34 -0400

    fix debug, image size preview

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@ import './styles/app.css';
 //import * as bootstrap from 'bootstrap';
 import * as tabler from '@tabler/core';
 import '@tabler/core/dist/css/tabler.min.css';
+import '@andypf/json-viewer';
 
 window.bootstrap = tabler.bootstrap;
 
